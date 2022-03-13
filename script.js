3f3714b40fa17d47823af42bc250cc9ac4916372
erm0es <Emelie.rosendahl@hotmail.se>
2022-03-13 10:38:59 +0100

    fisrt commit

--- script.js
+++ script.js
@@ -6,14 +6,12 @@ let base =[
 "målar",
 "jogga",
 "sover",
-"cykla"
+"slita"
 ];
 
-//the guessing word, split to read every letter
 let guessing="hallå";
 let guessingWord = guessing.toLowerCase().split("");
 
-//takes a random word from the base and slits the word
 let rightGuess = base[Math.floor(Math.random() * base.length)];
 let wordBase = rightGuess.toLowerCase().split("");
 
@@ -21,28 +19,24 @@ console.log(wordBase);
 console.log(guessingWord);
 
 const holder = [];
-const checkDubble = [];
 
 function checkGuesses (){
-    //an array with obj for output
-    for(let i = 0; i < wordBase.length; i++){
-        //checks position of letters is correct
+    for(let i = 0; i < 5; i++){
+
         let position = wordBase.indexOf(guessingWord[i]);
-        //for the different results
+        
         if(guessingWord[i] === wordBase[i]){
             holder[i] = {letter: guessingWord[i], result: "correct"};
-            checkDubble.push(guessingWord[i]);
         }else{
             holder[i] = {letter: guessingWord[i], result: "missplaced"};
         }
         if(position === -1){
             holder[i] = {letter: guessingWord[i], result: "incorrect"};
         }
+        wordBase[position] = "";
+        
        
     };  
     console.log(holder)
-    console.log(checkDubble)
 }
 checkGuesses();
-
-//if letter is doubble
